3dbc52ae72ee892aa3e62910253bb830686c0c19
ahmedsgit <acc.ahmedsazzad@gmail.com>
2022-02-15 17:06:40 +0600

    sidebar component

--- src/components/appBody/Layout.js
+++ src/components/appBody/Layout.js
@@ -26,6 +26,7 @@ const Container = styled.div`
         display: flex;
         flex-direction: column;
         padding: 0 5px;
+        margin: 5px 20px;
     }
 `;
 
--- src/components/appBody/Sidebar.js
+++ src/components/appBody/Sidebar.js
@@ -1,7 +1,12 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
 import styled from 'styled-components';
 import backgroundPic from '../../images/card-bg.svg';
+import itemIcon from '../../images/item-icon.svg';
 import addPhoto from '../../images/photo.svg';
+import premiumIcon from '../../images/premium-icon.svg';
+import widgetIcon from '../../images/widget-icon.svg';
+import plusIcon from '../../images/plus-icon.svg';
 
 
 function Sidebar() {
@@ -24,15 +29,39 @@ function Sidebar() {
                             <span>Connections</span>
                             <span>Grow your network</span>
                         </div>
-                        <img src="" alt="" />
+                        <img src={widgetIcon} alt="plus icon" />
                     </a>
                 </Widget>
+                <Premium>
+                    <p>Access exclusive tools &amp; insights</p>
+                    <div>
+                        <img src={ premiumIcon} alt="premium icon" />
+                        <h6>Get Hired faster,try premium free</h6>
+                    </div>
+                </Premium>
                 <Item>
                     <span>
-                        <img src="" alt="" />
+                        <img src={itemIcon} alt="plus icon" />
+                        My Items
                     </span>
                 </Item>
             </ArtCard>
+            <CommunityCard>
+                <a>
+                    <span>Groups</span>
+                </a>
+                <a>
+                    <span>
+                        Events
+                        <img src={plusIcon} alt="" />
+                    </span>
+                </a>
+                <a>
+                    <span>Follow Hashtags</span>
+                </a>
+                <div className='border-bottom'/>
+                <button>Discover more</button>
+            </CommunityCard>
         </Container>
     )
 }
@@ -97,7 +126,157 @@ const AddPhotoText = styled.div`
     font-weight: 400;
 `;
 
-const Widget = styled.div``;
-const Item = styled.a``;
+const Widget = styled.div`
+    border-bottom: 1px solid rgba( 0, 0, 0, 0.15 );
+    & > a {
+        text-decoration:none;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 15px 12px;
+
+        &:hover {
+            background-color: rgba( 0, 0, 0, 0.08 );
+        }
+
+        div {
+            display: flex;
+            flex-direction: column;
+            text-align: left;
+            span {
+                font-size: 13px;
+                font-weight: 600;
+                line-height: 1.33;
+                &:first-child {
+                    color: rgba( 0, 0, 0, 0.6 );
+                }
+                &:nth-child(2) {
+                    color: rgba( 0, 0, 0, 1 );
+                }
+            }
+        }
+    }
+
+    svg {
+        color: rgba( 0, 0, 0, 1 );
+    }
+`;
+
+const Premium = styled.div`
+    cursor: pointer;
+    display: flex;
+    flex-direction: column;
+    border-bottom: 1px solid rgba( 0, 0, 0, 0.15 );
+    p{
+        padding-top: 12px;
+        font-size: smaller;
+        color: rgba( 0, 0, 0, 0.5 );
+    }
+    div {
+        display: flex;
+        align-items: center;
+        margin: 5px auto;
+        padding-bottom: 12px;
+
+        img{
+            margin-right: 5px;
+        }
+
+        h6 {
+            color: rgba( 0, 0, 0, 0.8 );
+            font-size: 13px;
+            text-transform: capitalize;
+        }
+    }
+    &:hover {
+        background-color: rgba( 0, 0, 0, 0.08 );
+    }
+`;
+
+const Item = styled.a`
+    border-color: rgba( 0, 0, 0, 0.8 );
+    text-align: left;
+    padding: 12px;
+    font-size: 12px;
+    display: block;
+    span {
+        display: flex;
+        align-items:center;
+        font-weight: 600;
+        color: rgba( 0, 0, 0, 1 );
+        svg {
+            color: rgba( 0, 0, 0, 0.6 );
+        }
+    }
+    &:hover {
+        background-color: rgba( 0, 0, 0, 0.08 );
+    }
+`;
+
+const CommunityCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 5px;
+    overflow: hidden;
+    margin-bottom: 8px;
+    background-color: #fff;
+    border-radius: 5px;
+    transition: box-shadow 83ms;
+    position: relative;
+    border: none;
+    color: rgba(0, 0, 200, 0.7);
+    font-weight: 600;
+    font-size: 12px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+
+    a {
+        text-decoration: none;
+        margin: 5px 0 5px 10px;
+        max-width: fit-content;
+
+        span {
+            cursor: pointer;
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: space-between;
+
+            img {
+                cursor: pointer;
+                padding: 5px;
+                border-radius: 10px;
+                transition: .3s ease-in-out;
+            }
+            img:hover {
+                background-color: rgba( 0, 0, 0, 0.15 );
+                padding: 5px;
+            }
+        }
+        &:nth-child(2){
+            max-width: 100%;
+        }
+    }
+
+    .border-bottom{
+        border-bottom: 1px solid rgba( 0, 0, 0, 0.15 );
+        margin: 3px;
+    }
+    button {
+        cursor: pointer;
+        background-color: transparent;
+        border: none;
+        color: rgba( 0, 0, 0, 0.6 );
+        font-weight: 600;
+        padding: 10px 0;
+        &:hover {
+            background-color: rgba( 0, 0, 0, 0.15 );
+            border-radius: 5px;
+        }
+        &:active {
+            transform: scale(0.95);
+        }
+    }
+
+`;
 
 export default Sidebar
